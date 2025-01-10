721a51007ed5338318f68bbc9a96b48f6bb78a60
Giann Pabon <giannalberto2@outlook.com>
2025-01-10 03:36:03 -0400

    extract parse_line function into its own file

---
+++ parse_line.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+#define BUFFER_SIZE 1024
+
+char **parse_line(char *line)
+{
+	size_t bufsize = BUFFER_SIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, " \t\r\n");
+	while (token)
+	{
+		tokens[position] = token;
+		position++;
+		if (position >= bufsize)
+		{
+			bufsize += BUFFER_SIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, " \t\r\n");
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
+
