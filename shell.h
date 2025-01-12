55a464e05358e8003efc7a7a1026674f6d0c7905
Giann Pabon <giannalberto2@outlook.com>
2025-01-12 18:32:07 -0400

    Update header file for new file structure

--- shell.h
+++ shell.h
@@ -9,14 +9,22 @@
 #include <sys/types.h>
 
 /* Function prototypes */
+
+/* Environment functions */
 char *fetch_env_var(char **env_vars, const char *var_name);
-char *locate_command(char **env_vars, char *cmd);
-void run_program(char **env_vars, char **arguments, int *status_code);
 void display_environment(char **env_vars);
-int manage_builtin(char **arguments, char **env_vars, int *status_code);
+
+/* I/O and memory functions */
 void split_input(char *input, char **args);
-char *fetch_command_path(char **env_vars, char *cmd, int *status_code);
 void release_memory(char **array);
 
-#endif
+/* Path functions */
+char *locate_command(char **env_vars, char *cmd);
+char *fetch_command_path(char **env_vars, char *cmd, int *status_code);
+
+/* Core functions */
+void run_program(char **env_vars, char **arguments, int *status_code);
+int manage_builtin(char **arguments, char **env_vars, int *status_code);
+
+#endif /* SHELL_H */
 
